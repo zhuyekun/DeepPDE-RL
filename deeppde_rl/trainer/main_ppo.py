6be4a307bcf89f857ce739bc7b6dd1fbf45a38e6
zhuyekun <zhuyekun123@sjtu.edu.cn>
2025-04-12 06:24:19 +0000

    Add support for Ray integration and FSDP strategy in PPO training. Introduced TaskRunner class for configuration handling, updated ppo.yaml with actor rollout reference, and added fsdp_workers for device mesh management.

--- deeppde_rl/trainer/main_ppo.py
+++ deeppde_rl/trainer/main_ppo.py
@@ -1,5 +1,8 @@
 import os
 import hydra
+import ray
+
+
 def get_reward_function(config):
     reward_fn_config = config.get("reward_function") or {}
     file_path = reward_fn_config.get("path")
@@ -35,5 +38,41 @@ def main(config):
 
 def run_ppo(config):
     os.environ["ENSURE_CUDA_VISIBLE_DEVICES"] = os.environ.get('CUDA_VISIBLE_DEVICES', '')
+    if not ray.is_initialized():
+        # this is for local ray cluster
+        ray.init(runtime_env={
+            'env_vars': {
+                # 'TOKENIZERS_PARALLELISM': 'true',
+                'NCCL_DEBUG': 'WARN',
+                'VLLM_LOGGING_LEVEL': 'WARN'
+            }
+        })
 
-    
+    runner = TaskRunner.remote()
+    ray.get(runner.run.remote(config))
+
+
+class TaskRunner:
+    def run(self, config):
+        from pprint import pprint
+        from omegaconf import OmegaConf
+        pprint(OmegaConf.to_container(config, resolve=True))  # resolve=True will eval symbol values
+        OmegaConf.resolve(config)
+
+        local_path = config.actor_rollout_ref.model.path
+
+        # TODO: instantiate tokenizer
+
+        # TODO: add other strategy
+        # define worker classes
+        if config.actor_rollout_ref.actor.strategy == 'fsdp':
+            assert config.actor_rollout_ref.actor.strategy == config.critic.strategy
+            # from verl.workers.fsdp_workers import ActorRolloutRefWorker, CriticWorker
+            # from verl.single_controller.ray import RayWorkerGroup
+            # ray_worker_group_cls = RayWorkerGroup
+
+        else:
+            raise ValueError(f"Invalid strategy: {config.actor_rollout_ref.actor.strategy}")
+
+
+
